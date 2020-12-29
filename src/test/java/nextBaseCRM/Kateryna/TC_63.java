e28bb735437a9ae2d179fc92127251ce5c852dd4
KaterynaHromova <kateryna.hromova1@gmail.com>
2020-12-29 14:10:14 -0600

    TC-63

--- src/test/java/nextBaseCRM/Kateryna/TC_63.java
+++ src/test/java/nextBaseCRM/Kateryna/TC_63.java
@@ -1,7 +1,33 @@
 package nextBaseCRM.Kateryna;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TC_63 {
     public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://login2.nextbasecrm.com/stream/?login=yes");
+
+        // log in
+        driver.findElement(By.xpath("//input[@name='USER_LOGIN']")).sendKeys("marketing49@cybertekschool.com");
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys("UserUser");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
+
+        //search box suggested modules verification
+
+        WebElement searchBoxElement=driver.findElement(By.xpath("//*[@name='q']"));
+        searchBoxElement.sendKeys("document");
+
+        if(driver.getPageSource().contains("Menu items") && driver.getPageSource().contains("Conversations") && driver.getPageSource().contains("Other") && driver.getPageSource().contains("Search") ){
+            System.out.println("Verification PASS");
+        }else{
+            System.out.println("Verification FAIL");
+        }
 
     }
 }
